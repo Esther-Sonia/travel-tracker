1aaad4cfd5c0b9b947454f8fd261f671f8d37d2a
Esther-Sonia <esthersonia21@gmail.com>
2025-05-26 15:45:45 +0300

    Add SQLAlchemy models for User, Destination, and Trip

---
+++ app/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow,nullable=False )
+
+class Destination(Base):
+    __tablename__ = 'destinations'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    country = Column(String)
+    continent = Column(String)
+
+    trips = relationship("Trip", back_populates="destination")
+
+class Trip(Base):
+    __tablename__ = 'trips'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    destination_id = Column(Integer, ForeignKey('destinations.id'))
+    start_date = Column(Date)
+    end_date = Column(Date)
+    notes = Column(String)
+
+    user = relationship("User", back_populates="trips")
+    destination = relationship("Destination", back_populates="trips")
+
